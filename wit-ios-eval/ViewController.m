7292ee3b12601add0f3cfecf2982ed8644e94e5a
Aric Lasry <lasry.aric@gmail.com>
2014-10-21 15:20:11 -0700

    Better UI for wit-ios-eval

--- wit-ios-eval/ViewController.m
+++ wit-ios-eval/ViewController.m
@@ -13,7 +13,10 @@
 @end
 
 @implementation ViewController {
-    UILabel *labelView;
+    UILabel *statusView;
+    UILabel *intentView;
+    UITextView *entitiesView;
+    WITMicButton* witButton;
 }
 
 - (void)viewDidLoad {
@@ -22,19 +25,34 @@
     
     // set the WitDelegate object
     [Wit sharedInstance].delegate = self;
-    
+    [self setupUI];
+}
+
+-(void)setupUI {
     // create the button
     CGRect screen = [UIScreen mainScreen].bounds;
     CGFloat w = 100;
     CGRect rect = CGRectMake(screen.size.width/2 - w/2, 60, w, 100);
     
-    WITMicButton* witButton = [[WITMicButton alloc] initWithFrame:rect];
+    witButton = [[WITMicButton alloc] initWithFrame:rect];
     [self.view addSubview:witButton];
     
     // create the label
-    labelView = [[UILabel alloc] initWithFrame:CGRectMake(0, 200, screen.size.width, 50)];
-    labelView.textAlignment = NSTextAlignmentCenter;
-    [self.view addSubview:labelView];
+    intentView = [[UILabel alloc] initWithFrame:CGRectMake(0, 200, screen.size.width, 50)];
+    intentView.textAlignment = NSTextAlignmentCenter;
+    entitiesView = [[UITextView alloc] initWithFrame:CGRectMake(0, 250, screen.size.width, screen.size.height - 300)];
+    entitiesView.backgroundColor = UIColorFromRGB(0xF4F4F4);
+    [self.view addSubview:entitiesView];
+    [self.view addSubview:intentView];
+    intentView.text = @"Intent will show up here";
+    entitiesView.textAlignment = NSTextAlignmentCenter;
+    entitiesView.text = @"Entities will show up here";
+    entitiesView.editable = NO;
+    entitiesView.font = [UIFont systemFontOfSize:17];
+    
+    statusView = [[UILabel alloc] initWithFrame:CGRectMake(0, 150, screen.size.width, 50)];
+    statusView.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:statusView];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -51,6 +69,20 @@
     labelView.text = [NSString stringWithFormat:@"intent = %@", intent];
     
     [self.view addSubview:labelView];
+}
+
+- (void)witActivityDetectorStarted {
+    statusView.text = @"Just listening... Waiting for voice activity";
+}
+
+-(void)witDidStartRecording {
+    statusView.text = @"Witting...";
+    entitiesView.text = @"";
+}
+
+- (void)witDidStopRecording {
+    statusView.text = @"Processing...";
+    entitiesView.text = @"";
 }
 
 @end
