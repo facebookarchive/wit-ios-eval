d1317ea43e7ae13f146ff0b4473493abb290582c
Aric Lasry <lasry.aric@gmail.com>
2014-10-21 15:23:55 -0700

    Add UIColorRGB macro

--- wit-ios-eval/ViewController.h
+++ wit-ios-eval/ViewController.h
@@ -9,6 +9,9 @@
 #import <UIKit/UIKit.h>
 #import <Wit/Wit.h>
 
+#define UIColorFromRGB(rgbValue) [UIColor colorWithRed:((float)((rgbValue & 0xFF0000) >> 16))/255.0 green:((float)((rgbValue & 0xFF00) >> 8))/255.0 blue:((float)(rgbValue & 0xFF))/255.0 alpha:1.0]
+
+
 @interface ViewController : UIViewController <WitDelegate>
 
 
